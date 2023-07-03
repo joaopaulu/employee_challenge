9e89a8b5455b75cd503f9a23d7b2931df3d41717
joaopaulu <jptick@gmail.com>
2023-07-03 12:24:05 -0300

    Criando o banco de dados e configurando o Migrations Flyway

---
+++ backend/src/main/resources/db/migration/V003__create_employee.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS  tb_servidor  (
+     id INT PRIMARY KEY AUTO_INCREMENT,
+     nome VARCHAR(60) NULL,
+     matricula INT NULL,
+     lotacao VARCHAR(45) NULL,
+     foto VARCHAR(255) NULL,
+     categoria_id  INT NOT NULL,
+  INDEX  fk_tb_servidor_tb_categoria_idx  ( categoria_id  ASC) VISIBLE,
+  CONSTRAINT  fk_tb_servidor_tb_categoria
+    FOREIGN KEY ( categoria_id )
+    REFERENCES  basisemployee . tb_categoria  ( id )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
